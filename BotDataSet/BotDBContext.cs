ca1029f64db63bcd29bdbcaefe36e1e57ff19ba3
Dether D <Dether-dether@yandex.ru>
2021-07-27 11:02:48 +0300

    DataSet

---
+++ BotDataSet/BotDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotDataSet
+{
+    class BotDBContext : DbContext
+    {
+        public BotDBContext() : base()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=BotDataSet.sqlite");
+        }
+        public DbSet<BotUser> Users { get; set; }
+    }
+}
