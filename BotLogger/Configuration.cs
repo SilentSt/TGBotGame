94b991a38ec33544e510b7f905293cad4eb54c5d
m.pervushin <m.pervushin@fast-report.com>
2021-07-30 17:26:21 +0300

    bot logger

---
+++ BotLogger/Configuration.cs
@@ -0,0 +1,7 @@
+namespace BotLogger
+{
+    public static class Configuration
+    {
+        public static readonly string BotToken = "";
+    }
+}
---
+++ BotLogger/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using TGBotGame;
+
+namespace BotLogger
+{
+    class Program
+    {
+        private static TelegramBotClient? Bot;
+
+        public static async Task Main()
+        {
+            Bot = new TelegramBotClient(Configuration.BotToken);
+
+            var me = await Bot.GetMeAsync();
+            Console.Title = me.Username;
+
+            using var cts = new CancellationTokenSource();
+
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            Bot.StartReceiving(new DefaultUpdateHandler(Handler.HandleUpdateAsync, Handler.HandleErrorAsync),
+                cts.Token);
+
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+
+            // Send cancellation request to stop bot
+            cts.Cancel();
+        }
+    }
+}
