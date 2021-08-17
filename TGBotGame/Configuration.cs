003cea0c99e6fc93a3f9254e2146c402957a9adc
m.pervushin <m.pervushin@fast-report.com>
2021-08-17 18:09:42 +0300

    12

--- TGBotGame/Configuration.cs
+++ TGBotGame/Configuration.cs
@@ -10,7 +10,7 @@ namespace TGBotGame
         public static readonly string RulesLink = File.ReadAllText("configuration/rules_chat_link.txt");
         public static readonly string QiwiToken = File.ReadAllText("configuration/qiwi_token.txt");
         public static readonly string QiwiMobile = File.ReadAllText("configuration/qiwi_phone.txt");
-        public static long GroupId = long.Parse(File.ReadAllText("configuration/group_id.txt"));
+        //public static long GroupId = long.Parse(File.ReadAllText("configuration/group_id.txt"));
         public static long AdminsGroupId = long.Parse(File.ReadAllText("configuration/admins_group_id.txt"));
     }
 }
